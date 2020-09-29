fce343e38d2acc0cb5257256a2719dd000aa5ab7
David Vicente <dvicente@full-on-net.com>
2020-09-29 16:05:58 +0200

    Jest for unit testing. One unit test

---
+++ tests/controllers/reverse.test.ts
@@ -0,0 +1,9 @@
+//import { Options } from '../../src/controllers/reverse.ts';
+
+import { expect } from 'chai';
+
+describe('Reverse tests', () => {
+    it('Simple reverse', () => {
+        expect(false).to.be.false;
+    });
+});
