805203509c6f31d4a082455bc54896c36117ed51
David Vicente <dvicente@full-on-net.com>
2020-09-29 17:59:53 +0200

    Reverse unit tests

---
+++ src/controllers/reverse.ts
@@ -0,0 +1,3 @@
+export function reverse(toReverse: string): string {
+    return '';
+};
--- tests/controllers/reverse.test.ts
+++ tests/controllers/reverse.test.ts
@@ -1,9 +1,34 @@
-//import { Options } from '../../src/controllers/reverse.ts';
-
+import * as reverseController from '../../src/controllers/reverse';
 import { expect } from 'chai';
 
 describe('Reverse tests', () => {
     it('Simple reverse', () => {
-        expect(false).to.be.false;
+        const reversed = reverseController.reverse('hello');
+        expect(reversed).to.equal('OllEh');
+    });
+
+    it('Reverse empty', () => {
+        const reversed = reverseController.reverse('');
+        expect(reversed).to.equal('');
+    });
+
+    it('Reverse only constants', () => {
+        const reversed = reverseController.reverse('nwmpyjhgf');
+        expect(reversed).to.equal('fghjypmwn');
+    });
+
+    it('Reverse only vowels', () => {
+        const reversed = reverseController.reverse('aeiou');
+        expect(reversed).to.equal('UOIEA');
+    });
+
+    it('Reverse long string', () => {
+        const reversed = reverseController.reverse('qwertyuioàsdfghjklzxcvbnm');
+        expect(reversed).to.equal('mnbvcxzlkjhgfdsApOIUytrEwq');
+    });
+
+    it('Reverse one letter string', () => {
+        const reversed = reverseController.reverse('a');
+        expect(reversed).to.equal('A');
     });
 });
